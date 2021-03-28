87ba21a53884b18e2f5abd11c367e109f65395d6
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-28 07:50:02 -0400

    Completed Lesson 7

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody theRB;
+    public float maxSpeed;
+
+    void Start()
+    {
+        theRB.transform.parent = null;
+    }
+
+    void Update()
+    {
+        theRB.AddForce(new Vector3(0f, 0f, 100f));
+        transform.position = theRB.position;
+    }
+}
