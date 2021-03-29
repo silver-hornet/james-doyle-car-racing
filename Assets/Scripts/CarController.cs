fd28810362e06c5672a6cbf006dae0dd92649f13
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-29 07:41:16 -0400

    Completed Lesson 8

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,6 +6,9 @@ public class CarController : MonoBehaviour
 {
     public Rigidbody theRB;
     public float maxSpeed;
+    public float forwardAccel = 8f;
+    public float reverseAccel = 4f;
+    float speedInput;
 
     void Start()
     {
@@ -14,7 +17,22 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
-        theRB.AddForce(new Vector3(0f, 0f, 100f));
+        speedInput = 0;
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel;
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel;
+        }
+
         transform.position = theRB.position;
+    }
+
+    void FixedUpdate()
+    {
+        theRB.AddForce(new Vector3(0f, 0f, speedInput));
     }
 }
