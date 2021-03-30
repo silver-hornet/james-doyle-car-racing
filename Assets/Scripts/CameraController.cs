6c249bff7b751a83a6a67c98ab85c02acee7111d
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-30 08:49:11 -0400

    Completed Lesson 23

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CarController target;
+    Vector3 offsetDir;
+
+    public float minDistance = 20f;
+    public float maxDistance = 50f;
+    float activeDistance;
+
+    void Start()
+    {
+        offsetDir = transform.position - target.transform.position;
+        activeDistance = minDistance;
+        offsetDir.Normalize();
+    }
+
+    void Update()
+    {
+        activeDistance = minDistance + ((maxDistance - minDistance) * (target.theRB.velocity.magnitude / target.maxSpeed));
+        transform.position = target.transform.position + (offsetDir * activeDistance);
+    }
+}
---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public GameObject[] cameras;
+    int currentCam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            currentCam++;
+
+            if (currentCam >= cameras.Length)
+            {
+                currentCam = 0;
+            }
+
+            for (int i = 0; i < cameras.Length; i++)
+            {
+                if (i == currentCam)
+                {
+                    cameras[i].SetActive(true);
+                }
+                else
+                {
+                    cameras[i].SetActive(false);
+                }
+            }
+        }
+    }
+}
