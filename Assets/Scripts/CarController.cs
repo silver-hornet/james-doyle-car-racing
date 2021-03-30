3402dc906e0ce7e50766fa88ddac9f93b28e28a2
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-30 08:09:33 -0400

    Completed Lesson 19

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -21,6 +21,10 @@ public class CarController : MonoBehaviour
     public Transform leftFrontWheel;
     public Transform rightFrontWheel;
     public float maxWheelTurn = 25f;
+    public ParticleSystem[] dustTrail;
+    public float maxEmission = 25f;
+    public float emissionFadeSpeed = 50f;
+    float emissionRate;
 
     void Start()
     {
@@ -51,7 +55,27 @@ public class CarController : MonoBehaviour
         // turning the wheels
         leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn) - 180, leftFrontWheel.localRotation.eulerAngles.z);
         rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
+
         transform.position = theRB.position;
+
+        // control particle emissions
+        emissionRate = Mathf.MoveTowards(emissionRate, 0f, emissionFadeSpeed * Time.deltaTime);
+
+        if (grounded && (Mathf.Abs(turnInput) > 0.5f || (theRB.velocity.magnitude < maxSpeed * 0.5f && theRB.velocity.magnitude != 0)))
+        {
+            emissionRate = maxEmission;
+        }
+
+        if (theRB.velocity.magnitude <= 0.5f)
+        {
+            emissionRate = 0f;
+        }
+
+        for (int i = 0; i < dustTrail.Length; i++)
+        {
+            var emissionModule = dustTrail[i].emission;
+            emissionModule.rateOverTime = emissionRate;
+        }
     }
 
     void FixedUpdate()
