613c64b93d5407e151afc224593c9da27c108177
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-31 07:51:45 -0400

    Completed Lesson 27

---
+++ Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    public AudioSource soundToPlay;
+    public int groundLayerNo = 8; // This layer is "Ground"
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer != groundLayerNo)
+        {
+            soundToPlay.Stop();
+            soundToPlay.pitch = Random.Range(0.8f, 1.2f);
+            soundToPlay.Play();
+        }
+    }
+}
