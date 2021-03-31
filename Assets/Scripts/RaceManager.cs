42ef487527ab46106972c91376d64ac518b877dd
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-03-31 08:29:56 -0400

    Completed Lesson 30

---
+++ Assets/Scripts/RaceManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+    public Checkpoint[] allCheckpoints;
+
+    void Start()
+    {
+        for (int i = 0; i < allCheckpoints.Length; i++)
+        {
+            allCheckpoints[i].cpNunber = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
