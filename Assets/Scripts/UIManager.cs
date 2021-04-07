ced8018ec6f398b12e9afd3a59d85a2b1039ad7a
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-07 07:30:00 -0400

    Completed Lesson 59

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,6 +14,8 @@ public class UIManager : MonoBehaviour
     public TMP_Text goText;
     public TMP_Text raceResultText;
     public GameObject resultsScreen;
+    public GameObject pauseScreen;
+    public bool isPaused;
 
     void Awake()
     {
@@ -22,16 +24,41 @@ public class UIManager : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        isPaused = !isPaused;
+        pauseScreen.SetActive(isPaused);
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
     }
 
     public void ExitRace()
     {
+        Time.timeScale = 1f;
         RaceManager.instance.ExitRace();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit");
     }
 }
