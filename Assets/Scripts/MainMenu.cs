2640db818a37e5825ef4f94b7fc4d4f720ddd45b
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-08 07:41:33 -0400

    Completed Lesson 64

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject raceSetupPanel;
+    public GameObject trackSelectPanel;
+    public GameObject racerSelectPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene(RaceInfoManager.instance.trackToLoad);
@@ -14,5 +18,39 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Game Quit");
+    }
+
+    public void OpenRaceSetup()
+    {
+        raceSetupPanel.SetActive(true);
+    }
+
+    public void CloseRaceSetup()
+    {
+        raceSetupPanel.SetActive(false);
+    }
+
+    public void OpenTrackSelect()
+    {
+        trackSelectPanel.SetActive(true);
+        CloseRaceSetup();
+    }
+
+    public void CloseTrackSelect()
+    {
+        trackSelectPanel.SetActive(false);
+        OpenRaceSetup();
+    }
+
+    public void OpenRacerSelect()
+    {
+        racerSelectPanel.SetActive(true);
+        CloseRaceSetup();
+    }
+
+    public void CloseRacerSelect()
+    {
+        racerSelectPanel.SetActive(false);
+        OpenRaceSetup();
     }
 }
