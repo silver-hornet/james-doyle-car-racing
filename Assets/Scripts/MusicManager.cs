c41327236a7a9ade04ac5b6b63344ff4ecead91c
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2021-04-09 08:40:02 -0400

    Completed Lesson 70

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource musicPlayer;
+    public AudioClip[] potentialMusic;
+
+    void Start()
+    {
+        musicPlayer.clip = potentialMusic[Random.Range(0, potentialMusic.Length)];
+        musicPlayer.Play();
+    }
+}
